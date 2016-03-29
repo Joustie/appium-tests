45a6c1bda238630ccd286f8be413a51accd3e134
Joost Evertse <joustie@gmail.com>
2016-03-29 23:29:10 +0200

    Changed upload script

--- upload_todo.sh
+++ upload_todo.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-curl -u : -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/joustie/todo.app.zip?overwrite=true --data-binary @/Users/joostevertse/tmp/blogpost/todomvcios/platforms/ios/build/emulator/TodoMVCiOS.zip
+
+#change these variables
+SAUCE_USERNAME=sauceuser
+SAUCE_ACCESS_KEY=saucekey
+#make sure the files are zipped (ususally just zip the .app dir on the mac)
+REMOTE_FILE=remotefile
+LOCAL_FILE=localfile
+
+curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/joustie/$REMOTE_FILE?overwrite=true --data-binary @$LOCAL_FILE
 
